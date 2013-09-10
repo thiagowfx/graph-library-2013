358f3d9c9a9a292a566f9f3cb541e63fb06de2f7
thiagowfx <thiagoperrotta95@gmail.com>
2013-09-10 00:06:16 -0300

    comecei a fazer dfs

--- Teoria dos Grafos/graph.cpp
+++ Teoria dos Grafos/graph.cpp
@@ -21,7 +21,9 @@ void graph::read_graph(char* filename) {
     getline(input_file, line);
     sscanf(line.c_str(), "%d", &n);
 
+    // aloca as estruturas de dados
     madj = vector< vector<bool> >(n + 1, vector<bool>(n + 1, false));
+    ladj = vector< vector<int> >(n + 1, vector<int>());
 
     // lê as arestas
     m = 0;
@@ -29,7 +31,14 @@ void graph::read_graph(char* filename) {
       ++m;
       sscanf(line.c_str(), "%d%d", &u, &v);
 
+      // atualiza matriz de adjacência
       madj[u][v] = madj[v][u] = true;
+
+      // atualiza lista de adjacência
+      // [@@@]
+      ladj[u].push_back(v);
+      ladj[v].push_back(u);
+      
     }
     
     input_file.close();
@@ -67,4 +76,24 @@ int graph::degree(int u) {
   for (int i = 1; i <= n; ++i)
     d += madj[u][i]; // d+= madj[i][u]
   return d;
+}
+
+void graph::dfs(int v) {
+  // [!!!]
+}
+
+void graph::dfs_wrapper(char* filename, int source) {
+
+  ofstream output_file;
+  output_file.open(filename);
+
+  // [@@@] - zerar as estruturas de modo conveniente
+  visited = vector<bool>(n + 1, false);
+  pai = vector<int>(n + 1, -1);
+  
+  // [!!!] - chamar dfs
+  // [!!!] - imprimir a saída de modo adequado
+
+
+  output_file.close();
 }
--- Teoria dos Grafos/graph.h
+++ Teoria dos Grafos/graph.h
@@ -1,7 +1,11 @@
+// [!!!] wishlist --> para implementar
+// [@@@] gambiarra ou stub ou implementação temporária --> para modificar, melhorar depois
+
 #ifndef GRAPH_H
 #define GRAPH_H
 
 #include <algorithm>
+#include <cstring>
 #include <iomanip>
 #include <iostream>
 #include <fstream>
@@ -13,13 +17,20 @@ using namespace std;
 // vértices começam em 1
 class graph {
 
+  // estruturas básicas para o grafo
+  
   int n,			// número de nós
     m;				// número de arestas
   
   vector< vector<bool> > madj;	// matriz de adjacência: true = conectado
   vector< vector<int> > ladj;	// lista de adjacência
 
+  // estruturas acessórias para outros métodos dfs, bfs
   vector<bool> visited;
+  vector<int> pai;
+
+  // depth-first search a partir do nó especificado
+  void dfs(int);
 
  public:
 
@@ -31,6 +42,7 @@ class graph {
   graph(string);
 
   // retorna o grau de um nó
+  // via matriz de adjacência -- O(n)
   int degree(int);
 
   // lê o grafo de um arquivo
@@ -40,7 +52,11 @@ class graph {
   void generate_info(char*);
 
   // retorna o grau médio do grafo
+  // O(1)
   double average_degree();
+
+  // depth-first search a partir de um nó, com info para o arquivo especificado
+  void dfs_wrapper(char*, int);
  
 };
 
